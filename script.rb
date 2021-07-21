3590cbd851a10fba0f2ce44ec70711fdbb7f7291
ctrlVee <me123opportunity@gmail.com>
2021-07-21 16:28:18 -0400

    Appends max & min to list

--- script.rb
+++ script.rb
@@ -4,10 +4,30 @@ require 'pry-byebug'
 
 @price_list = [17,3,6,9,15,8,6,1,10]
 def stock_picker(array)
-  binding.pry
+ 
   @min = array.min
   @max = array.max
- 
+  print_result
+  get_index()
+end
+
+def get_index
+  @best_buy_sell = []
+  @price_list.each_index do |price_index|
+    case price_index
+    when @price_list.index(@min)
+      @best_buy_sell.append(price_index)
+    when @price_list.index(@max)
+      @best_buy_sell.append(price_index)
+    end
+  end
+  @best_buy_sell
+  binding.pry
+end
+
+
+def print_result
+  puts "The profit is #{@max} - #{@min} = #{@max-@min}."
 end
 
 stock_picker(@price_list)
